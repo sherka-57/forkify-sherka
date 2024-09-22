1307d59683d611a11a3ed85d2a45756b29e22bb2
Shermukhammad Sattarov <shersattarov0711@gmail.com>
2024-09-22 15:39:28 -0400

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 11;
+export const KEY = '7e245872-eab9-48fc-84d0-0b2c7e1a46f0';
+export const MODAL_CLOSE_SEC = 2.5;
