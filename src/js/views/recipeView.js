84f42938df6d40db11bdfb660df1502afc6d5c9c
sherka <154922248+Ibn-al-waqt@users.noreply.github.com>
2024-09-26 13:03:15 -0400

    Update recipeView.js

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 
 import icons from 'url:../../img/icons.svg';
-import {fracty} from 'fracty';
+import {Fracty} from 'fracty';
 
 class RecipeView extends View {
     _parentElement = document.querySelector('.recipe');
@@ -122,7 +122,7 @@ class RecipeView extends View {
             <svg class="recipe__icon">
             <use href="${icons}#icon-check"></use>
             </svg>
-            <div class="recipe__quantity">${ing.quantity ? fracty(ing.quantity).toString(): ''}</div>
+            <div class="recipe__quantity">${ing.quantity ? Fracty(ing.quantity).toString(): ''}</div>
             <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
             ${ing.description}
